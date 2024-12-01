f36faea9b8f8f4679b2915368888048c81bbf304
Paakallo <paviveerar@gmail.com>
2024-12-01 19:40:14 +0100

    add game loop and window

---
+++ src/GameWindow.java
@@ -0,0 +1,15 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends Canvas {
+    private JFrame frame;
+    private Dimension size;
+
+    
+    public GameWindow(int width, int height, String title, Main game){
+        
+    }
+
+}
