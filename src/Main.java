89174ebaed131d89a2cedbbc6eb3de7cb703f677
Paakallo <paviveerar@gmail.com>
2024-12-02 20:38:55 +0100

    fixes

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,12 @@ public class Main extends Canvas {
 
     static boolean isRunning = true;
 
+    private ObjectHandler handler;
+
     public Main(){
         new GameWindow(window_width,window_height,"HopeSkill",this);
-        Player player = new Player(30,30,(float) window_width,(float) window_height, 1);
+        //Player player = new Player(30,30,(float) window_width,(float) window_height, 1);
+        handler = new ObjectHandler();
     }
 
     public static void main(String[] args) {
@@ -30,12 +33,19 @@ public class Main extends Canvas {
         }
     }
 
+    //tick through all game objects
+    private void tick(){
+        handler.tick();
+    }
+
     // render function (I aassume it renders current map)
     public void paint(Graphics g){
 
         //somehow there will be map
         g.setColor(Color.RED);
         g.fillRect(50, 50, 100, 100);
+
+        handler.render(g);
     }
 
     
@@ -46,33 +56,33 @@ public class Main extends Canvas {
     public static int getWindowWidth(){
         return window_width;
     }
-    Player player = new Player(30,30,(float) window_width,(float) window_height, 1);
+    //Player player = new Player(30,30,(float) window_width,(float) window_height, 1);
 
 
 
-class KeysPressed extends KeyAdapter{
+// class KeysPressed extends KeyAdapter{
 
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-                //jump
-                break;
-            //left
-            case KeyEvent.VK_A:
-                float x = player.getX();
-                player.setX(player.getX() - player.getVel_x());
-                break;
-            //button for action
-            case KeyEvent.VK_S:
-                break;
-            //right    
-            case KeyEvent.VK_D:
-                player.setX(player.getX() + player.getVel_x());
-                break;
-            default:
-                break;
-        }
-    }          
-}
+//     public void keyPressed(KeyEvent e) {
+//         switch (e.getKeyCode()) {
+//             case KeyEvent.VK_W:
+//                 //jump
+//                 break;
+//             //left
+//             case KeyEvent.VK_A:
+//                 float x = player.getX();
+//                 player.setX(player.getX() - player.getVel_x());
+//                 break;
+//             //button for action
+//             case KeyEvent.VK_S:
+//                 break;
+//             //right    
+//             case KeyEvent.VK_D:
+//                 player.setX(player.getX() + player.getVel_x());
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }          
+// }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,23 +1,69 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class Player extends GameObject{
     private boolean[] mov_keys = new boolean[4];
 
-    float height = 32;
-    float width = 10;
+    static float height = 32;
+    static float width = 16;
     public Player(float x, float y, int scale) {
         super(x, y, width, height, scale);
-        setVel_X(5);
-        setVel_y(5);
+
+        //setVel_X(5);
+        //setVel_y(5);
     }
 
     void render(Graphics g){
-        
+        g.setColor(Color.yellow);
+        g.fillRect((int)getX(), (int)getY(), (int) width, (int) height);
+        showBounds(g);
     }
 
-    void gravity(){
-        setVel_y(-3);
+    public void tick(){
+        //update position every tick
+        setX(getVel_x()+getX());
+        setY(getVel_y()+getY());
     }
+
+    public Rectangle getBounds(){
+        return new Rectangle((int) (getX()+getWidth()/2-getWidth()/4), 
+        (int) (getY()+getHeight()/2), 
+        (int) getWidth()/2, 
+        (int) getHeight()/2);
+    }
+
+    public Rectangle getBoundsTop(){
+        return new Rectangle((int) (getX()+getWidth()/2-getWidth()/4), 
+        (int) getY(), 
+        (int) getWidth()/2, 
+        (int) getHeight()/2);
+    }
+
+    public Rectangle getBoundsRight(){
+        return new Rectangle((int) (getX()+getWidth()/2 - 5), 
+        (int) getY() + 5, 
+        5, 
+        (int) getHeight() - 10);
+    }
+
+    public Rectangle getBoundsLeft(){
+        return new Rectangle((int) getX(), 
+        (int) getY() + 5, 
+        5, 
+        (int) getHeight() - 10);
+    }
+
+    private void showBounds(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+
+        g.setColor(Color.red);
+        g2d.draw(getBounds());
+        g2d.draw(getBoundsRight());
+        g2d.draw(getBoundsLeft());
+        g2d.draw(getBoundsTop());
+    }
+
     public boolean[] getKeys(){
         return this.mov_keys;
     }
