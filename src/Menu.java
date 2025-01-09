0f6374dab0890819c172fa11207d11bf2bd92c4a
Paakallo <paviveerar@gmail.com>
2025-01-09 17:03:59 +0100

    select game logic

--- src/Menu.java
+++ src/Menu.java
@@ -14,6 +14,7 @@ public class Menu extends MouseAdapter {
     //graphics
     private BufferedImage backgroundImage;
     private BufferedImage playButton;
+    private BufferedImage levelButton;
 
     static int BUTTON_WIDTH = 200;
     static int BUTTON_HEIGHT = 100;
@@ -29,6 +30,9 @@ public class Menu extends MouseAdapter {
 
     int RETURN_BUTTON_HEIGHT = 50;
 
+    int SELECT_LEVEL_WIDTH = 165;
+    int SELECT_LEVEL_HEIGHT = 100;
+
     public Menu(Main game) {
         this.game = game;
 
@@ -41,6 +45,12 @@ public class Menu extends MouseAdapter {
         try {
             // Load the background image
             playButton = ImageIO.read(new File("resources/play_button.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            // Load the background image
+            levelButton = ImageIO.read(new File("resources/open_level.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -62,17 +72,7 @@ public class Menu extends MouseAdapter {
                 renderStartMenu(g);
         
             } else {
-                g.setColor(Color.WHITE);
-                g.setFont(new Font("Arial", Font.PLAIN, 30));
-
-                g.drawRect(510, 300, 200, 50);
-                g.drawString("1", 530, 335);
-
-                g.drawRect(100, 100, 200, 50);
-                g.drawString("Level 2", 120, 135);
-
-                g.drawString("Return", RETURN_BUTTON_X + 20, RETURN_BUTTON_Y + 35);
-
+                renderChooseLevel(g);
             }
 
         } else { //draw in-game menu bar
@@ -100,41 +100,61 @@ public class Menu extends MouseAdapter {
             g.drawString("Quit", PLAY_BUTTON_X + 20, PLAY_BUTTON_Y + 135);
     }
 
+    // render what level to choose
+    public void renderChooseLevel(Graphics g){
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.PLAIN, 30));
+
+        g.drawString("Return", RETURN_BUTTON_X + 20, RETURN_BUTTON_Y + 35);
+
+        for (int i = 1;i<6;i++){
+            g.drawImage(levelButton, SELECT_LEVEL_WIDTH * i, PLAY_BUTTON_Y, SELECT_LEVEL_WIDTH, SELECT_LEVEL_HEIGHT, null);
+        }
+    }
+
     @Override
     public void mousePressed(MouseEvent e) {
         int mx = e.getX();
         int my = e.getY();
 
-        
-        // check if quit button
-        if (mx >= PLAY_BUTTON_X && mx <= PLAY_BUTTON_X + BUTTON_WIDTH && my >= PLAY_BUTTON_Y + 100 && my <= PLAY_BUTTON_Y + 100 + BUTTON_HEIGHT) {
-            
-            System.exit(0);
-            
-        } 
 
         if (game.getGameState() == GameState.MENU) {
+             // check if quit button
+            if (mx >= PLAY_BUTTON_X && mx <= PLAY_BUTTON_X + BUTTON_WIDTH && my >= PLAY_BUTTON_Y + 100 && my <= PLAY_BUTTON_Y + 100 + BUTTON_HEIGHT) {
+                System.exit(0);
+            } 
+
+
             // Check if "Start Game" is clicked
             if (mx >= PLAY_BUTTON_X && mx <= PLAY_BUTTON_X + BUTTON_WIDTH && my >= PLAY_BUTTON_Y && my <= PLAY_BUTTON_Y + BUTTON_HEIGHT) {
                 startGameClicked = true;
                 
             }
-
-            if (mx >= 100 && mx <= 300 && my >= 100 && my <= 150 && startGameClicked) {
-                game.poziom2();
-                startGameClicked = false;
-                
+            
+            //return button
+            if (mx >= RETURN_BUTTON_X && mx <= BUTTON_WIDTH && my >= RETURN_BUTTON_Y && my <= RETURN_BUTTON_HEIGHT) {  
+                game.setGameState(GameState.MENU); // switch to menu
+                startGameClicked = false; // in case button was clicked in the menu
             }
+
             // level 1 clicked
-            if (mx >= 0 && mx <= 200 && my >= 0 && my <= 350 && startGameClicked) {
+            if (mx >= SELECT_LEVEL_WIDTH && mx <= 2 * SELECT_LEVEL_WIDTH && my >= PLAY_BUTTON_Y && my <= PLAY_BUTTON_Y + SELECT_LEVEL_HEIGHT && startGameClicked) {
                 game.poziom1();
                 startGameClicked = false;
                 
             }
+
+            if (mx >= 2 * SELECT_LEVEL_WIDTH && mx <= 4 * SELECT_LEVEL_WIDTH && my >= PLAY_BUTTON_Y && my <= PLAY_BUTTON_Y + SELECT_LEVEL_HEIGHT && startGameClicked) {
+                game.poziom2();
+                startGameClicked = false;
+                
+            }
+        
         } else {
-            // return button
+            // return button in game
             if (mx >= RETURN_BUTTON_X && mx <= BUTTON_WIDTH && my >= RETURN_BUTTON_Y && my <= RETURN_BUTTON_HEIGHT) {  
                 game.setGameState(GameState.MENU); // switch to menu
+                startGameClicked = false; // in case button was clicked in the menu
             }
 
         }
